7aa9fd7faee9b2630b1ca552fdc029061f8582a1
grassel82 <78179056+grassel82@users.noreply.github.com>
2024-06-24 09:16:36 +0200

    Add files via upload

---
+++ messenger-app/app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Configure MySQL from environment variables
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
+
+app_port = os.environ.get('EXPOSED_PORT','5000')
+app_IP = os.environ.get('EXPOSED_IP','0.0.0.0')
+
+# Initialize MySQL
+mysql = MySQL(app)
+
+@app.route('/')
+def hello():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT message FROM messages')
+    messages = cur.fetchall()
+    cur.close()
+    return render_template('index.html', messages=messages)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    new_message = request.form.get('new_message')
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO messages (message) VALUES (%s)', [new_message])
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('hello'))
+
+if __name__ == '__main__':
+    app.run(host=app_IP, port=app_port, debug=True)
+
